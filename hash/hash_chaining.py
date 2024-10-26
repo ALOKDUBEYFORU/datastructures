9601a5cc3ebce5e544ab0db359a705b938dae221
Alok Dubey <DUBEYALOK@REDIFFMAIL.COM>
2024-10-26 10:29:54 +0530

    changed the chaining mechanism
    fixed a bug where in the same string was getting inserted in a different node, if we are trying to update the value and if the rehashing was happening at the same time.

--- hash/hash_chaining.py
+++ hash/hash_chaining.py
@@ -53,6 +53,25 @@ class LinkedList:
             temp = temp.next
             counter = counter + 1
 
+    def ll_remove(self,key):
+        if self.head != None:
+            curr_node = self.head
+            if curr_node.key == key:
+                self.head = self.head.next
+                return 'deleted'
+
+            while curr_node != self.tail:
+                if curr_node.next == self.tail and curr_node.next.key == key:
+                    curr_node.next = None
+                    self.tail = curr_node
+                    return 'deleted'
+
+                if curr_node.next.key == key :
+                    curr_node.next = curr_node.next.next
+                    return 'deleted'
+                curr_node = curr_node.next
+        return 'key not found'
+
 
     def len(self):
         return self.llsize
@@ -91,10 +110,11 @@ class Dictionary:
 
     def put(self,key,value):
         bucket_index = self.hash_function(key)
+        self.dictelements = self.dictelements + 1
 
         #let us calculate the load factor.
         load_factor = self.dictelements / self.capacity
-        print(load_factor)
+
         if load_factor >= 2 :
             self.rehash(self.capacity)
 
@@ -105,7 +125,7 @@ class Dictionary:
         else:
             node = self.buckets[bucket_index].get_node_at_index(node_index)
             node.value = value
-        self.dictelements = self.dictelements + 1
+
 
     def __str__(self):
         dstr = ""
@@ -115,14 +135,36 @@ class Dictionary:
                 dstr = dstr + str(self.buckets[i])+'<-->'
         return dstr[:-4]
 
+    def get(self,key):
+        if self.dictelements > 0 :
+            for i in range(self.capacity):
+                for j in range(self.buckets[i].len()):
+                    if self.buckets[i].get_node_at_index(j).key == key:
+                        return 'found in bucket '+str(i)+' at node - '+str(j)
+        return "No elements exists"
+
+    def remove(self,key):
+        if self.dictelements >0 :
+            for i in range(self.capacity):
+                is_deleted = self.buckets[i].ll_remove(key)
+                print(is_deleted)
+                if is_deleted == 'deleted':
+                    return 'deleted'
+        return 'key not found'
+
+
+
 if __name__ == "__main__":
     dict1 = Dictionary()
-    dict1.put('python',40)
-    dict1.put('c', 400)
-    dict1.put('c++',500)
-    dict1.put('java', 600)
-    print(dict1)
-    dict1.put('python', 50)
-    print(dict1)
-
-
+    print('inserting Python with a value of 40',dict1.put('python',40))
+    print('inserting c with a value of 400',dict1.put('c', 400))
+    print('inserting c++ with a value of 500',dict1.put('c++',500))
+    print('inserting java with a value of 600',dict1.put('java', 600))
+    print('printing dictionary after insertions',dict1)
+    print('insert of update the value of key python with 50',dict1.put('python', 50))
+    print('printing dictionary after insertion/updation of key python',dict1)
+    print('print the value of the key python',dict1.get('python'))
+    print('delete the key python',dict1.remove('python'))
+    print('printing dictionary after removal of string python',dict1)
+    print('delete the key c',dict1.remove('c'))
+    print('printing dictionary after removal of string c',dict1)
